9bd98920087d4e758d36bcc1c15c394c0f11e2d3
Eduardo Wingrove <Binario01101@gmail.com>
2023-06-12 16:24:01 -0600

    Update 3

--- calculadora-react/src/App.js
+++ calculadora-react/src/App.js
@@ -1,9 +1,79 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import Boton from './componentes/Botones';
+import Pantalla from './componentes/Pantalla';
+import BotonClear from './componentes/BotonClear';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
+
+  const [input, setinput] =useState('');
+
+  const agregarInput = val => {
+
+    setinput(input + val);
+
+  };
+
+  const calcularResultado = () => {
+
+    setinput(evaluate(input))
+
+  }
+
   return (
     <div className="App">
-    hola
+    
+    <div className='freecodecamp-logo-contenedor'>
+
+      <img
+      alt='Logo de Free Code Camp'
+      src={freeCodeCampLogo}
+      className='freecodecamp-logo'
+      />
+
+    </div>
+
+      <div className='contenedor-calculadora'>
+
+        <Pantalla input={input}>
+
+        </Pantalla>
+
+        <div className='fila'>
+          <Boton manejarClick={agregarInput}>1</Boton>
+          <Boton manejarClick={agregarInput}>2</Boton>
+          <Boton manejarClick={agregarInput}>3</Boton>
+          <Boton manejarClick={agregarInput}>+</Boton>
+        </div>
+
+        <div className='fila'>
+          <Boton manejarClick={agregarInput}>4</Boton>
+          <Boton manejarClick={agregarInput}>5</Boton>
+          <Boton manejarClick={agregarInput}>6</Boton>
+          <Boton manejarClick={agregarInput}>-</Boton>
+        </div>
+
+        <div className='fila'>
+          <Boton manejarClick={agregarInput}>7</Boton>
+          <Boton manejarClick={agregarInput}>8</Boton>
+          <Boton manejarClick={agregarInput}>9</Boton>
+          <Boton manejarClick={agregarInput}>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={calcularResultado}>=</Boton>
+          <Boton manejarClick={agregarInput}>0</Boton>
+          <Boton manejarClick={agregarInput}>.</Boton>
+          <Boton manejarClick={agregarInput}>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear manejarClear={ () => setinput('')} > 
+          Limpiar
+          </BotonClear>
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ calculadora-react/src/componentes/Pantalla.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../hojas-estilo/Pantalla.css';
+
+const Pantalla = ({ input }) => {
+  return (
+    <div className='input'>
+
+    {input}
+
+    </div>
+  )
+}
+
+export default Pantalla
